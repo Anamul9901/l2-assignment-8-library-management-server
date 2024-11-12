7f3a38e0ccc4a9970c6ff5b50c94ee738f31120e
Anamul Haque <anamulhaque9901@gmail.com>
2024-11-13 02:27:05 +0600

    Over due days api created

--- src/app/modules/Borrow-Return/borrow-return.routes.ts
+++ src/app/modules/Borrow-Return/borrow-return.routes.ts
@@ -1,10 +1,12 @@
-import express from 'express'
-import { BorrowReturnController } from './borrow-return.controllre';
+import express from "express";
+import { BorrowReturnController } from "./borrow-return.controllre";
 
 const router = express.Router();
 
-router.post('/borrow', BorrowReturnController.borrowBookFromDB)
+router.post("/borrow", BorrowReturnController.borrowBookFromDB);
 
-router.post('/return', BorrowReturnController.returnBookToDB);
+router.post("/return", BorrowReturnController.returnBookToDB);
+
+router.get("/borrow/overdue", BorrowReturnController.overDueBookShow);
 
 export const BorrowReturnRoutes = router;
--- src/app/modules/Borrow-Return/borrow-return.service.ts
+++ src/app/modules/Borrow-Return/borrow-return.service.ts
@@ -11,9 +11,9 @@ const createBorrow = async (data: BorrowRecord) => {
 
   await prisma.member.findUniqueOrThrow({
     where: {
-        memberId: data.memberId
-    }
-  })
+      memberId: data.memberId,
+    },
+  });
 
   const result = await prisma.borrowRecord.create({ data });
   return result;
@@ -37,7 +37,42 @@ const returnBook = async (borrowId: string) => {
   return result;
 };
 
+const overDueBook = async () => {
+  const result = await prisma.borrowRecord.findMany({
+    where: {
+      returnDate: null,
+    },
+    include: {
+      book: true,
+      member: true,
+    },
+  });
+  const today = new Date();
+  const finalResult = result.map((record) => {
+    const { borrowId, borrowDate, book, member } = record;
+
+    const borrowDateTime = new Date(borrowDate);
+    const timeDiference = today.getTime() - borrowDateTime.getTime();
+    const dayDiference = Math.floor(timeDiference / (1000 * 60 * 60 * 24));
+    const overdueDays = dayDiference - 14;
+
+    if (overdueDays > 0) {
+      return {
+        borrowId,
+        bookTitle: book.title,
+        borrowerName: member.name,
+        overdueDays,
+      };
+    } else {
+      return null;
+    }
+  }).filter(data => data != null);
+
+  return finalResult;
+};
+
 export const BorrowReturnService = {
   createBorrow,
   returnBook,
+  overDueBook,
 };
